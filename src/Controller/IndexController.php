88c8e08204add2551031185b72e2f4b874e96a1e
baich <baich97oussama@gmail.com>
2019-11-04 13:57:43 +0100

    filter Commit

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\Repository\ProfRepository;
 use App\Repository\EleveRepository;
 use App\Repository\ClasseRepository;
+use App\Repository\NiveauRepository;
 use App\Repository\ParentsRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,16 +14,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="index", methods={"GET"})
      */
-    public function index(ClasseRepository $classe, ProfRepository $prof, EleveRepository $eleve, ParentsRepository $parents)
+    public function index(ClasseRepository $classe, ProfRepository $prof, EleveRepository $eleve, ParentsRepository $parents, NiveauRepository $niveau)
     {
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
             'classe' => $classe->findAll(),
             'prof' => $prof->findAll(),
             'eleve' => $eleve->findAll(),
-            'parents' => $parents->findAll()
+            'parents' => $parents->findAll(),
+            'niveau' => $niveau->findAll(),
         ]);
     }
 }
